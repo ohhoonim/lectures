760b810a66c338c05e7a33d3d81896259febbbcd
hdju <donnj95@gmail.com>
2018-03-28 19:16:37 +0900

    init

--- startCoding/src/exam/BubbleSort.java
+++ startCoding/src/exam/BubbleSort.java
@@ -1,5 +1,7 @@
 package exam;
 
+import java.util.Arrays;
+
 public class BubbleSort {
 	public static void main(String[] args) {
 		int[] arr = {3, 4, 5, 2, 1};
@@ -17,7 +19,7 @@ public class BubbleSort {
 					isChanged = true;
 				}
 			}
-			// 마지막 반복에서 자리 바꿈이 있었는지를 체크하고 없으면 break;
+			// 마지막 반복에서 자리 바꿈이 없었는지를 체크하고 없으면 break;
 			if (!isChanged ) {
 				break;
 			}
@@ -27,5 +29,6 @@ public class BubbleSort {
 		for (int i = 0; i < arr.length; i++) {
 			System.out.println(arr[i]);
 		}
+		
 	}
 }
